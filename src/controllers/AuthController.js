b06073efcec4af46ab796e4ab68cfc742becd566
shreyagargg <shreyagarg06031412@gmail.com>
2026-04-04 11:49:57 +0000

    auth logic and role based access scenes

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,11 +1,49 @@
-const Login = (req, res) => {
+import bcrypt from 'bcrypt.js'
+import jwt from 'jsonwebtoken'
+
+const Login = async (req, res) => {
+  const {email, password} = req.body
+
+  const result = await pool.query(
+    'select * from users where email = $1',
+    [email]
+  )
+
+  const user = result.rows[0]
+  if(!user)
+      return res.status(404).json("no user")
+
+  const is_match = await bcrypt.compare(password, user.password)
+  if(!is_match)
+    return res.status(401).json("wrong password")
+
+  const token = jwt.sign(
+    {id: user.id, role: user.role},
+    process.env.JWT_SECRET,
+    {expiresIn: '1d'}
+  )
+
+  res.json({token})
+
   console.log("login called")
 
 }
 
-const Register = (req, res) => {
+const Register = async (req, res) => {
+  const {email, password, role} = req.body
+
+const hashed = await bcrypt.hash(password, 10)
+
+const result = await pool.query(
+    `insert INTO users (email, password, role)
+   VALUES ($1, $2, $3)
+   RETURNING id, email, role`,
+  [email, hashed, role || 'viewer']
+);
+
   console.log("register called")
 
+  res.status(201).json(result.rows[0])
 }
 
 export {Login, Register}
--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -4,7 +4,7 @@ import {pool} from "../config/db.js"
 const AuthMiddleware = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if(!token)
-    return res.status(401).json({"error"})
+    return res.status(401).json({ error: "No token provided" })
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
